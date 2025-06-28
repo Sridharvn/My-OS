e239e6910cce9c3897d69e14e2237db76011ce11
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-06-28 09:46:00 +0530

    Add protected mode assembly code for screen clearing and kernel jump

---
+++ boot/protected.asm
@@ -0,0 +1,18 @@
+bits 32
+section .text
+
+global protected_main()
+
+protected_main:
+    ; Clear the screen by writing spaces to video memory
+    mov edi, 0xb8000
+    mov ecx, 80*25
+    mov ax, 0x0720      ; Space character with attribute
+.clear:
+    stosw
+    loop .clear
+    ; Jump to kernel main (you will define this in C)
+    call 0x1000     ; Kernel is loaded at 0x1000
+.halt:
+    hlt
+    jmp .halt
